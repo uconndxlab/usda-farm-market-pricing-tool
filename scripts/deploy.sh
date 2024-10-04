b0795069b748545a5310fb7c753400cf41f86b56
Brian Kelleher <brian.kelleher@uconn.edu>
2024-10-04 14:52:38 -0400

    can i get a node

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,15 +8,20 @@ cd usda-farm-market-pricing-tool
 
 DB_CONNECTION=sqlite composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
-# php artisan optimize
-
-# php artisan migrate --force
-
 chown -R :www-data .
+
+npm ci
+npm run build
 
 if [ -d /var/www/marketpricing.core.uconn.edu/ ]; then
     echo "Copying files to the server"
     rsync -arvp --omit-dir-times ./ /var/www/marketpricing.core.uconn.edu/
+
+    cd /var/www/marketpricing.core.uconn.edu
+    php artisan migrate --force
+    php artisan optimize
+    chown -R :www-data .
 else 
     echo "Skipping file copy. Prod directory does not exist."
 fi
+
