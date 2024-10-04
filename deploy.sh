c747e460b65447a2597786c32d8c2b6bc9eb047e
Brian Kelleher <brian.kelleher@uconn.edu>
2024-10-04 13:57:58 -0400

    test turnip deploy

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Which user is running this script?
+echo "Running as $(whoami)"
+
+cd usda-farm-market-pricing-tool
+
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+php artisan optimize
+
+php artisan migrate --force
+
+chown -R :www-data .
+
+if [ -d /var/www/marketpricing.core.uconn.edu/ ]; then
+    echo "Copying files to the server"
+    rsync -arvp --omit-dir-times ./ /var/www/marketpricing.core.uconn.edu/
+else 
+    echo "Skipping file copy. Prod directory does not exist."
+fi
